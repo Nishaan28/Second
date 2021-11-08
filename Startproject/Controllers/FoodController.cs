90797825a6ef2c28a9df5d343a4ffa1ad43887bc
sohel <Nisha>
2021-11-08 14:01:11 +0530

    Hii

---
+++ Startproject/Controllers/FoodController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Startproject.Controllers
+{
+    public class FoodController : Controller
+    {
+        // GET: Food
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string firstname, string lastname, string email)
+    }
+}
